b581a61f9ec311bf33c2117f7f391b8d0a8d4e15
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-08 19:23:58 +0100

    update projects list

--- src/entity/Project.ts
+++ src/entity/Project.ts
@@ -12,7 +12,7 @@ export class Project {
     @Column()
     price: number;
 
-    @Column({default: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjIxKyp8sTeAhUCmrQKHbZDBDwQjRx6BAgBEAU&url=http%3A%2F%2Fwww.uniqueserviceshub.com%2Fcareer_zone%2F&psig=AOvVaw0GGsop3_NUS8z0z4lX5LYu&ust=1541769805408220'})
+    @Column({default: 'http://www.bigfish.pk/images/company/defaultLogo.jpg'})
     photo: string
 
     @ManyToMany(type => User, user => user.projects)
