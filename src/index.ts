3274f81be6ccd51d549d6371f7557b77464b0a64
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-25 16:56:31 +0100

    update aws

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import appPromise from "./app"
 appPromise.then(({ app }) => {
     console.log('tutaj', process.env);
     
-    app.listen(process.env.PORT || 8002)
-    console.log(`app is running on port ${process.env.PORT || 8002}`)
+    app.listen(process.env.RDS_PORT || 8002)
+    console.log(`app is running on port ${process.env.RDS_PORT || 8002}`)
 }).catch(console.log)
