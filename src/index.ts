19fe5776e7a69aea1aa2226f56f73cca123f0949
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-25 16:53:50 +0100

    update index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import appPromise from "./app"
 
 appPromise.then(({ app }) => {
+    console.log('tutaj', process.env);
+    
     app.listen(process.env.PORT || 8002)
     console.log(`app is running on port ${process.env.PORT || 8002}`)
 }).catch(console.log)
