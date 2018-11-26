fccc466522e834873dd129517e6dc6d0a5cb70f5
Robert Mrowiec <rmrowiec@icloud.com>
2018-11-26 15:42:19 +0100

    update aws env

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,38 @@
 const path = require('path')
 
-module.exports = {
-    "type": "postgres",
-    "host": process.env.RDS_HOSTNAME || "localhost",
-    "port": process.env.RDS_PORT || 5432,
-    "username": process.env.RDS_USERNAME || "postgres",
-    "password": process.env.RDS_PASSWORD || "123",
-    "database": process.env.RDS_DB_NAME || "carsapp",
-    "synchronize": true,
-    "logging": false,
-    "entities": [
-        path.join(__dirname, 'src/entity/**/!(*.spec.js|*.spec.ts)'),
-    ],
-    "migrations": [
-        path.join(__dirname, 'src/entity/**/!(*.spec.js|*.spec.ts)'),
-    ]
+export function dbConfiguration (process, env) {
+    const obj = {
+        "type": "postgres",
+        "port": 5432,
+        "synchronize": true,
+        "logging": false,
+        "entities": [
+            path.join(__dirname, 'src/entity/**/!(*.spec.js|*.spec.ts)'),
+        ],
+        "migrations": [
+            path.join(__dirname, 'src/entity/**/!(*.spec.js|*.spec.ts)'),
+        ]
+
+    }
+    if (env == 'prod') {
+        return (
+            {
+                "host": process.RDS_HOST,
+                "username": process.RDS_USERNAME,
+                "password": process.RDS_PASSWORD,
+                "database": process.RDS_NAME,
+                "ssl": true,
+                ...obj
+            }
+        )
+    }
+    return (
+        {
+            "host": "localhost",
+            "username": "postgres",
+            "password": "123",
+            "database": "carsapp",
+            ...obj
+        }
+    )
 }
