29c76055f9a72cd034637116c896b37c034d1666
Robert Mrowiec <rmrowiec@icloud.com>
2018-12-05 18:12:20 +0100

    update unnecessary lines

--- src/tests/users.spec.ts
+++ src/tests/users.spec.ts
@@ -1,21 +1,13 @@
 import { UserController } from '../controllers/usersController';
-import { Repository, getRepository } from 'typeorm';
-import { User } from '../entity/User';
 import { expect } from 'chai';
 import appPromise from "../app";
 import 'mocha'
 
-const hello = () => 'Hello world!'; 
-
 describe('user Controller', () => {
     let controller: UserController
-    let appCallback
-    beforeEach(async () => {
-        return appPromise.then(({ app }) => {
-            appCallback = app.callback()
-            controller = new UserController()
-        })
-    })
+    beforeEach(() => appPromise.then(async () => {
+        return controller = new UserController()
+    }))
 
     it('Get users array', async () => {
         const res = await controller.get().catch(x => x)
